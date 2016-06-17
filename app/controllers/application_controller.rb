acce22f4c18b3e688436e3a7e16bcb803678399c
Galen Palmer <palmergs@gmail.com>
2016-06-16 22:32:34 -0400

    utility methods on application contorller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from 'ActiveRecord::RecordNotFound' do |exception|
+    render json: { errors: [ 'Record not found.' ] }, status: :not_found
+  end
+
+  rescue_from 'ActiveRecord::RecordNotUnique' do |exception|
+    render json: { errors: [ 'Duplicate keys not allowed.'] }, status: :unprocessable_entity
+  end
+
+  protected
+
+    def render_errors entity, status = :unprocessable_entity
+      if entity && entity.respond_to?(:errors)
+        render json: { errors: entity.errors.full_messages }, status: status
+      else
+        render_error nil, status
+      end
+    end
+
+    def render_error message, status = :unprocessable_entity
+      message ||= "An error occurred"
+      render json: { errors: [ message.to_s ] }, status: status
+    end
+
+    
 end
