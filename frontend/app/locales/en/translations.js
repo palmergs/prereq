05c03b93055ec7c2c118fdef27d489a737f5bd4e
Galen Palmer <palmergs@gmail.com>
2016-06-21 22:13:35 -0400

    add i18n support

---
+++ frontend/app/locales/en/translations.js
@@ -0,0 +1,17 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  "contact" : {
+    "label" : {
+      "cancel" : "Cancel",
+      "ok" : "OK"
+    }
+  }
+};
