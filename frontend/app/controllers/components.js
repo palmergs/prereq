f7756eb9d1de5567b23cd47a926b58729098c268
Galen Palmer <palmergs@gmail.com>
2016-06-22 10:46:55 -0400

    working with ember validations

--- frontend/app/controllers/components.js
+++ frontend/app/controllers/components.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(EmberValidations, {
   one: false,
   two: false,
   three: false,
@@ -8,16 +9,57 @@ export default Ember.Controller.extend({
   five: false,
   six: false,
 
+  firstName: null,
+
+  firstNameObserver: Ember.observer('firstName', function() {
+    console.log(this.get('firstName'));
+  }),
+
+  validations: {
+    firstName: {
+      presence: true,
+      length: {
+        minimum: 1,
+        maximum: 64
+      }
+    }
+  },
+
   progressValue: 1,
   progressValue1: 2,
   progressValue2: 2,
   progressValue3: 2,
+
+  updateProgressValues() {
+    setTimeout(() => {
+      this.set('progressValue', this.get('progressValue') + parseInt(Math.random() * 2));
+      this.set('progressValue1', this.get('progressValue1') + parseInt(Math.random() * 2));
+      this.set('progressValue2', this.get('progressValue2') + parseInt(Math.random() * 2));
+      this.set('progressValue3', this.get('progressValue3') + parseInt(Math.random() * 2));
+      this.updateProgressValues();
+    }, 2000);
+  },
 
   names: ['Stefan', 'Miguel', 'Tomster', 'Pluto'],
   name: 'Tomster',
 
   cities: ['Barcelona', 'London', 'New York', 'Porto'],
   selectedCities: [],
+
+  groupedNumbers: [
+    { groupName: "Smalls", options: ["one", "two", "three"] },
+    { groupName: "Mediums", options: ["four", "five", "six"] },
+    { groupName: "Bigs", options: [
+      { groupName: "Fairly big", options: ["seven", "eight", "nine"] },
+      { groupName: "Really big", options: [ "ten", "eleven", "twelve" ] },
+      "thirteen"]
+    },
+    "one hundred",
+    "one thousand"
+  ],
+
+  buttonState: "default",
+  downloadState: "primary",
 
   setAllFalse() {
     this.set('one', false);
@@ -30,6 +72,7 @@ export default Ember.Controller.extend({
 
   onInit: function() {
     this.setAllFalse();
+    this.updateProgressValues();
   }.on('init'),
 
   actions: {
@@ -40,6 +83,22 @@ export default Ember.Controller.extend({
       if (text.length > 0 && this.get('cities').indexOf(text) === -1) {
         this.get('selectedCities').pushObject(text);
       }
+    },
+    handleFocus(select, e) { select.actions.open(); },
+    download() { this.set("buttonState", "loading"); },
+    downloadPromise(value, e, callback) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        setTimeout(() => {
+          const val = Math.random();
+          if(val > 0.33) {
+            this.set('downloadState', 'success');
+            resolve("Finished");
+          } else {
+            this.set('downloadState', 'danger');
+            reject("Errored");
+          }
+        }, 2000);
+      });
     },
     submit() { this.setAllFalse(); },
     reset() { this.setAllFalse(); },
