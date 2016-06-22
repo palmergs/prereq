9835f8495d9150fe52cd0fa737e2652ae1fd5e91
Galen Palmer <palmergs@gmail.com>
2016-06-22 08:38:30 -0400

    add the ember-power-select

--- frontend/app/controllers/components.js
+++ frontend/app/controllers/components.js
@@ -13,6 +13,12 @@ export default Ember.Controller.extend({
   progressValue2: 2,
   progressValue3: 2,
 
+  names: ['Stefan', 'Miguel', 'Tomster', 'Pluto'],
+  name: 'Tomster',
+
+  cities: ['Barcelona', 'London', 'New York', 'Porto'],
+  selectedCities: [],
+
   setAllFalse() {
     this.set('one', false);
     this.set('two', false);
@@ -27,6 +33,14 @@ export default Ember.Controller.extend({
   }.on('init'),
 
   actions: {
+    foo(value) { this.set('name', value); },
+    handleKeydown(dropdown, e) {
+      if (e.keyCode !== 13) { return; }
+      let text = e.target.value;
+      if (text.length > 0 && this.get('cities').indexOf(text) === -1) {
+        this.get('selectedCities').pushObject(text);
+      }
+    },
     submit() { this.setAllFalse(); },
     reset() { this.setAllFalse(); },
     open1() { this.set('one', true); },
