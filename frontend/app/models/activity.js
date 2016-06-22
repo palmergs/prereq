33e736313b949041dbff92cc50ab05c46877476e
Galen Palmer <palmergs@gmail.com>
2016-06-22 17:19:52 -0400

    add time machine; associations for activity

--- frontend/app/models/activity.js
+++ frontend/app/models/activity.js
@@ -1,12 +1,21 @@
+import Ember from 'ember';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-import { hasMany } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   name: attr('string'),
   description: attr('string'),
+  status: attr('string'),
+  completed: attr('boolean'),
   createdAt: attr('date'),
 
   previousLinks: hasMany('link', { inverse: 'nextActivity', async: true }),
-  nextLinks: hasMany('link', { inverse: 'previousActivity', async: true })
+  previousActivities: Ember.computed.mapBy('previousLinks', 'previousActivity'),
+
+  nextLinks: hasMany('link', { inverse: 'previousActivity', async: true }),
+  nextActivities: Ember.computed.mapBy('nextLinks', 'nextActivity'),
+
+  parent: belongsTo('activity', { inverse: 'children', async: true }),
+  children: hasMany('activity', { inverse: 'parent', async: true })
 });
