ac2bed7003e0c8006a8ed0f05a2dd9103cad3f88
Galen Palmer <palmergs@gmail.com>
2016-06-21 21:47:14 -0400

    wrap last modal in boolean

--- frontend/app/controllers/components.js
+++ frontend/app/controllers/components.js
@@ -3,7 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     submit() {
-
+      this.set('one', false);
+      this.set('two', false);
+      this.set('three', false);
+      this.set('four', false);
+      this.set('five', false);
+      this.set('six', false);
     },
     toggle1() {
       this.toggleProperty('one');
