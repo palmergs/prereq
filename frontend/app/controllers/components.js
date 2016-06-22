da922972c933e04094f758912d333a5b5b49624c
Galen Palmer <palmergs@gmail.com>
2016-06-21 22:13:54 -0400

    support default reset action for modals

--- frontend/app/controllers/components.js
+++ frontend/app/controllers/components.js
@@ -1,32 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  one: false,
+  two: false,
+  three: false,
+  four: false,
+  five: false,
+  six: false,
+
+  setAllFalse() {
+    this.set('one', false);
+    this.set('two', false);
+    this.set('three', false);
+    this.set('four', false);
+    this.set('five', false);
+    this.set('six', false);
+  },
+
+  onInit: function() {
+    this.setAllFalse();
+  }.on('init'),
+
   actions: {
-    submit() {
-      this.set('one', false);
-      this.set('two', false);
-      this.set('three', false);
-      this.set('four', false);
-      this.set('five', false);
-      this.set('six', false);
-    },
-    toggle1() {
-      this.toggleProperty('one');
-    },
-    toggle2() {
-      this.toggleProperty('two');
-    },
-    toggle3() {
-      this.toggleProperty('three');
-    },
-    toggle4() {
-      this.toggleProperty('four');
-    },
-    toggle5() {
-      this.toggleProperty('five');
-    },
-    toggle6() {
-      this.toggleProperty('six');
-    },
+    submit() { this.setAllFalse(); },
+    reset() { this.setAllFalse(); },
+    open1() { this.set('one', true); },
+    open2() { this.set('two', true); },
+    open3() { this.set('three', true); },
+    open4() { this.set('four', true); },
+    open5() { this.set('five', true); },
+    open6() { this.set('six', true); },
   }
 });
