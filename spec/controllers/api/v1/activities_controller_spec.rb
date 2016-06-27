a1519cf9c4d99a4fc293e7fdfd5aa6d37a488227
Galen Palmer <palmergs@gmail.com>
2016-06-26 20:40:34 -0400

    spec for create activities

--- spec/controllers/api/v1/activities_controller_spec.rb
+++ spec/controllers/api/v1/activities_controller_spec.rb
@@ -15,7 +15,24 @@ RSpec.describe Api::V1::ActivitiesController, type: :controller do
   end
 
   describe '#create' do
+    let(:hash) {
+      {
+        'data' => {
+          'type' => 'activities',
+          'attributes' => {
+            'name' => 'Activity 1',
+            'description' => 'This is a new activity.'
+          }
+        }
+      }
+    }
 
+    it 'can create a new activity from a hash' do
+      expect {
+        post :create, hash
+        expect(response).to be_success
+      }.to change { Activity.count }.from(0).to(1)
+    end
   end
 
   describe '#update' do
