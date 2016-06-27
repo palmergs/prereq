d9b82ea924b1c1a2a329a3fa2ef4064331f1ee43
Galen Palmer <palmergs@gmail.com>
2016-06-27 19:38:03 -0400

    passing 'it exists' test for non-DS model

--- frontend/tests/unit/models/flash-message-test.js
+++ frontend/tests/unit/models/flash-message-test.js
@@ -1,12 +1,17 @@
-import { moduleForModel, test } from 'ember-qunit';
+import { moduleFor, test } from 'ember-qunit';
+// import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('flash-message', 'Unit | Model | flash message', {
-  // Specify the other units that are required for this test.
-  needs: []
-});
+// moduleForModel('flash-message', 'Unit | Model | flash message', {
+//   // Specify the other units that are required for this test.
+//   needs: []
+// });
+
+moduleFor('model:flash-message', 'Unit | Model | flash message');
 
 test('it exists', function(assert) {
-  let model = this.subject();
+  // let model = this.subject();
   // let store = this.store();
+
+  const model = this.subject();
   assert.ok(!!model);
 });
