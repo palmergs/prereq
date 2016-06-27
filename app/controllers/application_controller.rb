b4f03f58f8f5a7ff9e4a06cfbe522d4021045a9e
Galen Palmer <palmergs@gmail.com>
2016-06-26 20:37:43 -0400

    update to allow json+api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   rescue_from 'ActiveRecord::RecordNotFound' do |exception|
     render json: { errors: [ 'Record not found.' ] }, status: :not_found
@@ -27,5 +28,4 @@ class ApplicationController < ActionController::Base
       render json: { errors: [ message.to_s ] }, status: status
     end
 
-    
 end
