a5fe546008b2cdc7fbe5229e6ff0eb553f360219
Galen Palmer <palmergs@gmail.com>
2016-08-10 10:15:45 -0400

    added ember-leaflet with example code

--- frontend/app/controllers/links/link/index.js
+++ frontend/app/controllers/links/link/index.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  lat: 45.519743,
+  lng: -122.680522,
+  zoom: 10,
+
+  emberConfLocation: [45.528298, -122.662986],
+  hotel: [45.530891, -122.655504],
+
   actions: {
     saved(link) {
       this.transitionToRoute('links.link', link.get('id'));
